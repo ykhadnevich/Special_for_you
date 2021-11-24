f63242d5e2fafbad4fa7a0f0d51134ab20951820
Yan_chik <90506265+ykhadnevich@users.noreply.github.com>
2021-11-24 12:14:25 +0300

    Update 2-improved.py

--- 2-improved.py
+++ 2-improved.py
@@ -1,6 +1,9 @@
-def sum(num1, num2):
-  return num1 + num2
-first_num = 2
-second_num = 3
-print(sum(first_num, second_num))
-#realy improved
+def sum(*args):
+    total_sum = 0
+    for number in args:
+        total_sum += number
+    return total_sum
+
+
+print(sum(1, 5, 7, 10))
+#really good loop 
